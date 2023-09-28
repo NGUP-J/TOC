4b08dee7b06616d7230dd3884cd72d54a889257a
sonmodx <sonmodx@gmail.com>
2023-09-28 23:37:53 +0700

    Updated DFA state

---
+++ main.js
@@ -0,0 +1,87 @@
+import {
+  changeStateByDigit,
+  changeStateBySpecailLetters,
+  changeStateByUpperAlpha,
+  changeStateByWord,
+  digitSet,
+  lowerAlphaSet,
+  speicalCharacterSet,
+  upperAlphaSet,
+} from "./utils.js";
+
+document.getElementById("button").addEventListener("click", handleClick);
+const userInput = document.getElementById("input");
+const result = document.getElementById("result");
+
+let initialState = "q0";
+let states = [
+  "q0",
+  "q1",
+  "q2",
+  "q3",
+  "q4",
+  "q5",
+  "q6",
+  "q7",
+  "q8",
+  "q9",
+  "q10",
+  "trap",
+];
+let alphabets = [
+  ...upperAlphaSet(),
+  ...lowerAlphaSet(),
+  ...digitSet(),
+  ...speicalCharacterSet(),
+];
+let finalState = "q10";
+const date = new Date().getDate().toString();
+// transition
+const firstTransition = changeStateByUpperAlpha("q0", "q1");
+const secondTransition = changeStateByDigit("q1", "q2");
+const thirdTransition = changeStateBySpecailLetters("q2", "q3");
+const forthTransition = changeStateByWord("q3", "q4", "kietikul");
+const fifthTransition = changeStateByWord("q4", "q5", "toc");
+const sixthTransition = changeStateByWord("q5", "q6", date);
+const seventhTransition = changeStateByWord("q6", "q7", "ecc");
+const eightTransition = changeStateByWord("q7", "q8", "bangkok");
+const ninthTransition = changeStateByWord("q8", "q9", "1");
+const tenthTransition = changeStateByWord("q9", "q10", "chicken-dog");
+
+let transitions = {
+  ...firstTransition,
+  ...secondTransition,
+  ...thirdTransition,
+  ...forthTransition,
+  ...fifthTransition,
+  ...sixthTransition,
+  ...seventhTransition,
+  ...eightTransition,
+  ...ninthTransition,
+  ...tenthTransition,
+};
+
+function handleClick() {
+  function isAccepted() {
+    const word = userInput.value;
+    let currentState = initialState;
+    for (let char of word) {
+      if (
+        !states.includes(currentState) &&
+        !(char in transitions[currentState])
+      ) {
+        currentState = "trap";
+        return false;
+      }
+      if (!alphabets.includes(char)) return false;
+      currentState = transitions[currentState][char] || currentState;
+      console.log("_curr", currentState);
+      if (currentState === finalState) return true;
+    }
+  }
+  if (isAccepted()) {
+    result.innerHTML = "Accepted";
+  } else {
+    result.innerHTML = "Rejected";
+  }
+}
---
+++ utils.js
@@ -0,0 +1,114 @@
+const alphaSet = (dec) => {
+  return Array.from(Array(26)).map((_, i) => i + dec);
+};
+
+export const upperAlphaSet = () => {
+  return alphaSet(65).map((x) => String.fromCharCode(x));
+};
+
+export const lowerAlphaSet = () => {
+  return alphaSet(97).map((x) => String.fromCharCode(x));
+};
+
+export const digitSet = () => {
+  return Array.from(Array(10).keys()).map((num) => num.toString());
+};
+
+export const speicalCharacterSet = () => {
+  return [
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "-",
+    "+",
+    "=",
+    "{",
+    "}",
+    "[",
+    "]",
+    ";",
+    ":",
+    '"',
+    "'",
+    ",",
+    "<",
+    ".",
+    ">",
+    "/",
+    "\\",
+    "?",
+    "`",
+    "~",
+    "|",
+  ];
+};
+
+export const changeStateByUpperAlpha = (currentState, nextState) => {
+  const alpha = upperAlphaSet();
+  const localTransition = {};
+
+  alpha.forEach((alp) => {
+    localTransition[currentState] = {
+      ...localTransition[currentState],
+      [alp]: nextState,
+    };
+  });
+  return localTransition;
+};
+
+export const changeStateByDigit = (currentState, nextState) => {
+  const digit = digitSet();
+  const localTransition = {};
+
+  digit.forEach((dig) => {
+    localTransition[currentState] = {
+      ...localTransition[currentState],
+      [dig]: nextState,
+    };
+  });
+  return localTransition;
+};
+
+export const changeStateBySpecailLetters = (currentState, nextState) => {
+  const speicalLetter = speicalCharacterSet();
+  const localTransition = {};
+
+  speicalLetter.forEach((sl) => {
+    localTransition[currentState] = {
+      ...localTransition[currentState],
+      [sl]: nextState,
+    };
+  });
+  return localTransition;
+};
+
+export const changeStateByWord = (currentState, nextState, word) => {
+  const myWord = word.split("");
+  const lengthWord = word.length;
+  const localTransition = {};
+  let _subCurr = 0;
+  let _curr = currentState;
+
+  myWord.forEach((mw) => {
+    _subCurr += 1;
+    let _nextState = `${currentState}_${_subCurr}`;
+    if (_subCurr == lengthWord) _nextState = nextState;
+    localTransition[_curr] = {
+      ...localTransition[_curr],
+      [mw]: _nextState,
+      [mw.toUpperCase()]: _nextState,
+    };
+
+    _curr = `${currentState}_${_subCurr}`;
+  });
+  console.log(localTransition);
+  return localTransition;
+};
