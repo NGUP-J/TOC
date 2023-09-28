78ab75a07e341a2036fbb462d91639cf1b039c91
sonmodx <sonmodx@gmail.com>
2023-09-28 23:58:44 +0700

    Updated DFA

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import {
-    changeStateByDigit,
-    changeStateBySpecailLetters,
-    changeStateByUpperAlpha,
-    changeStateByWord,
-    digitSet,
-    lowerAlphaSet,
-    speicalCharacterSet,
-    upperAlphaSet,
+  changeStateByDigit,
+  changeStateBySpecailLetters,
+  changeStateByUpperAlpha,
+  changeStateByWord,
+  digitSet,
+  lowerAlphaSet,
+  speicalCharacterSet,
+  upperAlphaSet,
 } from "./utils.js";
 
 document.getElementById("button").addEventListener("click", handleClick);
@@ -15,28 +15,28 @@ const result = document.getElementById("result");
 
 let initialState = "q0";
 let states = [
-    "q0",
-    "q1",
-    "q2",
-    "q3",
-    "q4",
-    "q5",
-    "q6",
-    "q7",
-    "q8",
-    "q9",
-    "q10",
-    "trap",
+  "q0",
+  "q1",
+  "q2",
+  "q3",
+  "q4",
+  "q5",
+  "q6",
+  "q7",
+  "q8",
+  "q9",
+  "q10",
+  "trap",
 ];
 let alphabets = [
-    ...upperAlphaSet(),
-    ...lowerAlphaSet(),
-    ...digitSet(),
-    ...speicalCharacterSet(),
+  ...upperAlphaSet(),
+  ...lowerAlphaSet(),
+  ...digitSet(),
+  ...speicalCharacterSet(),
 ];
 let finalState = "q10";
 const date = new Date().getDate().toString();
-//sub transition
+// transition
 const firstTransition = changeStateByUpperAlpha("q0", "q1");
 const secondTransition = changeStateByDigit("q1", "q2");
 const thirdTransition = changeStateBySpecailLetters("q2", "q3");
@@ -49,35 +49,39 @@ const ninthTransition = changeStateByWord("q8", "q9", "1");
 const tenthTransition = changeStateByWord("q9", "q10", "chicken-dog");
 
 let transitions = {
-    ...firstTransition,
-    ...secondTransition,
-    ...thirdTransition,
-    ...forthTransition,
-    ...fifthTransition,
-    ...sixthTransition,
-    ...seventhTransition,
-    ...eightTransition,
-    ...ninthTransition,
-    ...tenthTransition,
+  ...firstTransition,
+  ...secondTransition,
+  ...thirdTransition,
+  ...forthTransition,
+  ...fifthTransition,
+  ...sixthTransition,
+  ...seventhTransition,
+  ...eightTransition,
+  ...ninthTransition,
+  ...tenthTransition,
 };
-console.log(firstTransition);
+
 function handleClick() {
-    function isAccepted() {
-        const word = userInput.value;
-        let currentState = initialState;
-
-        for (let char of word) {
-            if (!alphabets.includes(char)) return false;
-            currentState = transitions[currentState][char] || "trap";
-            console.log("_curr", currentState);
-            if (currentState === "trap") return false;
-        }
-
-        if (currentState === finalState) return true;
+  function isAccepted() {
+    const word = userInput.value;
+    let currentState = initialState;
+    for (let char of word) {
+      if (
+        !states.includes(currentState) &&
+        !(char in transitions[currentState])
+      ) {
+        currentState = "trap";
+        return false;
+      }
+      if (!alphabets.includes(char)) return false;
+      currentState = transitions[currentState][char] || currentState;
+      console.log("_curr", currentState);
+      if (currentState === finalState) return true;
     }
-    if (isAccepted()) {
-        result.innerHTML = "Accepted";
-    } else {
-        result.innerHTML = "Rejected";
-    }
+  }
+  if (isAccepted()) {
+    result.innerHTML = "Accepted";
+  } else {
+    result.innerHTML = "Rejected";
+  }
 }
